bd0d234528c32e77e6d3f4c2a17ba21eefb64eea
khaledsobuj <khaledsobuj65@gmail.com>
2024-03-20 13:28:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import DaisyNav from './Component/DaisyNav/DaisyNav'
+
+function App() {
+
+
+  return (
+    <>
+
+      <DaisyNav></DaisyNav>
+      <h1 className='text-7xl'>Vite + React</h1>
+
+
+    </>
+  )
+}
+
+export default App
